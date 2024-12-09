cfee6c2501a7966159a1d19c3bfc093570161660
Kamila Gadek <gadek@student.agh.edu.pl>
2024-12-09 20:46:47 +0100

    Day 1 - solution

--- day_1.py
+++ day_1.py
@@ -1,12 +1,27 @@
-list_1 = [3, 4, 2, 1, 3, 3]
 
-list_2 = [4, 3, 5, 3, 9, 3]
+list1 = []
+list2 = []
+total_distance = 0
 
-# 1. Sort list
-print(list_1)
+# 1. Read file
+f = open("input_day1.txt", "r")
+lines = f.readlines()
 
-# 2. Loop through list
+# 2. Split string into two lists
+for line in lines:
+  x, y = line.split()
+  list1.append(x.rstrip())
+  list2.append(y.rstrip())
 
-# 3. Calculate distace
+# 3. Sort list
+sorted_list1 = sorted(list1)
+sorted_list2 = sorted(list2)
 
-# 4. Add difference 
+# 4. Loop through list
+for list1, list2 in zip(sorted_list1, sorted_list2):   
+# 5. Calculate distace
+    distance = abs(int(list1)-int(list2))
+# 6. Add difference 
+    total_distance += distance
+
+print(total_distance)
