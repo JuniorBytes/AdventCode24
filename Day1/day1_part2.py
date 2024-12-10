abf2e0a442f3282a11881d19d9cc606fa50b258b
Kamila Gadek <gadek@student.agh.edu.pl>
2024-12-10 20:43:41 +0100

    Day 1 - solution (part 2)

---
+++ Day1/day1_part2.py
@@ -0,0 +1,25 @@
+similarity_score = 0
+list1 = []
+list2 = []
+
+with open("Day1/input_day1.txt", "r") as file:
+  for line in file:
+    x, y = line.split()
+    list1.append(x.rstrip())
+    list2.append(y.rstrip())
+
+# 1. Loop through the first  list
+for elem in list1:
+  if elem in list2:
+    occurancy = list2.count(elem)
+    similarity_score += occurancy*int(elem)
+print(similarity_score)
+
+
+
+
+
+
+
+
+
