54f08bd381a56a5cfaab651cb5352d4779752918
Kamila Gadek <gadek@student.agh.edu.pl>
2024-12-16 20:27:08 +0100

    Day 2 - solution (part 1)

--- Day2/day2.py
+++ Day2/day2.py
@@ -1,12 +1,14 @@
-helper_dict = {}
 safe_reports_count = 0
-with open('Day2/test_input.txt') as file:
+with open('Day2/input_day2.txt') as file:
   for line in file:
     line = line.split()
     test_list = [int(elem) for elem in line]
     if sorted(test_list) == test_list or test_list == sorted(test_list, reverse=True):
+      temp_list = []
       for i in range(len(test_list)-1):
         if abs(test_list[i] - test_list[i+1]) <= 3 and abs(test_list[i] - test_list[i+1]) >= 1:
+          temp_list.append(test_list[i])
+        if len(temp_list) > len(test_list)-2:
           safe_reports_count += 1
 print(safe_reports_count)
 
